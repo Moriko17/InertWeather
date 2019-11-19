de18390c5ec5aef15c1ee07ff34727afaa2333ae
Moriko17 <17moriko@gmail.com>
2019-11-19 13:14:46 +0300

    Upd app component work, enable cors on express

--- inert-weather-app/src/containers/AppContainer.js
+++ inert-weather-app/src/containers/AppContainer.js
@@ -9,19 +9,26 @@ import {
 import App from "../components/App/App";
 
 const fetchWeather = (dispatch, coord, id) => {
-  const API_KEY = "6f49e4f6bef37c3172dac3cae65a0ae6";
-  const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+  // const API_KEY = "6f49e4f6bef37c3172dac3cae65a0ae6";
+  // const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+  // const promise = axios({
+  // url: `${ROOT_URL}&lat=${coord.lat}&lon=${coord.lon}`,
+  //   method: "get"
+  // });
+  const ROOT_URL = "http://localhost:3030/weather/coordinates";
   const promise = axios({
-    url: `${ROOT_URL}&lat=${coord.lat}&lon=${coord.lon}`,
+    url: `${ROOT_URL}?lat=${coord.lat}&lon=${coord.lon}`,
     method: "get"
   });
   dispatch(fetchWeatherStart(id));
   return promise
     .then(res => {
+      console.log(res);
       dispatch(fetchWeatherSuccess(res.data, id));
       return res.data;
     })
     .catch(error => {
+      console.log(error);
       dispatch(fetchWeatherFail(error, id));
       return error;
     });
@@ -58,7 +65,4 @@ const mapDispatchToProps = dispatch => ({
   updateState: newState => dispatch(updateState(newState))
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- inert-weather-server/app.js
+++ inert-weather-server/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 require("dotenv/config");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const weatherRouter = require("./routes/weatherRoute");
 const favoriteRouter = require("./routes/favoriteRoute");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use("/weather", weatherRouter);
 app.use("/favorites", favoriteRouter);
 
@@ -19,7 +22,5 @@ mongoose.connect(
   },
   () => console.log("Connected to db")
 );
-
-app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(3030);
